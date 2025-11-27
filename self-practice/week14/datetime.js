3ff4e4e23a4fd608f9f72dab6a0eefb1efc10736
siravijbb <siravijbb@gmail.com>
2025-11-27 17:25:52 +0700

    add:specific date

--- self-practice/week14/datetime.js
+++ self-practice/week14/datetime.js
@@ -4,7 +4,13 @@ const month = now.getMonth();
 const day = now.getDate();
 const hour = now.getHours();
 const minute = now.getMinutes();
-const second = now.getSeconds();
+setTimeout(function(){
+    const second = now.getSeconds();
 
-console.log(now);
-console.log(year, month, day, hour, minute, second);
+    console.log(now);
+    console.log(year, month, day, hour, minute, second);
+}, 1000); // still act as get at that time
+
+
+const specificDate = new Date('2025-12-25T10:30:00');
+console.log(specificDate.toISOString());
