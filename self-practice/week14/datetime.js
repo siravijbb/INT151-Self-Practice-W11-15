261d4468744f58557cc3981044e439dc11ec8ae6
siravijbb <siravijbb@gmail.com>
2025-11-27 17:22:41 +0700

    add: try time now

---
+++ self-practice/week14/datetime.js
@@ -0,0 +1,10 @@
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth();
+const day = now.getDate();
+const hour = now.getHours();
+const minute = now.getMinutes();
+const second = now.getSeconds();
+
+console.log(now);
+console.log(year, month, day, hour, minute, second);
